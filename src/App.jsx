471a7bd2e6c6a56e00d1f244a0a683ebe5de0046
Eduarda Martins <eduarda07martins@gmail.com>
2024-06-25 09:54:53 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import SearchForm from './SearchForm';
+import ThemeToogle from './ThemeToogle';
+import Gallery from './Gallery';
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToogle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+
+export default App;
